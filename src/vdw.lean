c7d9dddbf16a1ebed841fcc3e952b3edcde4dafb
David E. Narváez <david.narvaez@computer.org>
2022-11-02 02:32:31 -0400

    Adding the PHP statement

---
+++ src/vdw.lean
@@ -0,0 +1,18 @@
+import data.finset.card
+import data.fintype.basic
+import algebra.big_operators.basic
+
+def arithprog(a b c:ℕ):Prop:=∃ k:ℕ, (b=a+k) ∧ (c=b+k)
+
+def vdW_prop (N : ℕ) (r : ℕ) : Prop := ∀ f : fin N → fin r, ∃ (a b c : fin N), (arithprog a b c) ∧ (f a = f b) ∧ (f b = f c)
+
+def fin_preim {α : Type} [decidable_eq α] {n : ℕ} (f : fin n → α) (i : α) : finset (fin n) := finset.filter (λ j, f j = i) (fin.fintype n).elems
+
+lemma pigeonhole_principle : ∀ m n : ℕ, ∀ f : (fin m) → (fin n), ∃ i : fin n, (fin_preim f i).card >= m / n :=
+begin
+intros,
+by_contra,
+simp at h,
+have h' : ((fin.fintype n).elems.bUnion (fin_preim f)).card = m,
+
+end
