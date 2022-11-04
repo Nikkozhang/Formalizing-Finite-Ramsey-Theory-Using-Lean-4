7282f640f96db024c91dc4c2bcf30a475ea03e1e
David E. Narváez <david.narvaez@computer.org>
2022-11-04 14:55:42 -0400

    Partial proof of vdW_prop

--- src/vdw.lean
+++ src/vdw.lean
@@ -6,7 +6,27 @@ def arithprog(a b c:ℕ):Prop:=∃ k:ℕ, (b=a+k) ∧ (c=b+k)
 
 def vdW_prop (N : ℕ) (r : ℕ) : Prop := ∀ f : ℕ → fin r, ∃ (a b c : fin N), (arithprog a b c) ∧ (f a = f b) ∧ (f b = f c)
 
-lemma vdW_monotone : ∀ n r, vdW_prop n r → ∀ m, n ≤ m → vdW_prop m r := sorry
+lemma vdW_monotone : ∀ n r, vdW_prop n r → ∀ m, n ≤ m → vdW_prop m r :=
+begin
+intros _ _ vdWn _ nleqm,
+unfold vdW_prop,
+intro,
+unfold vdW_prop at vdWn,
+have david : ∃ (a b c : fin n), arithprog ↑a ↑b ↑c ∧ f ↑a = f ↑b ∧ f ↑b = f ↑c,
+apply vdWn,
+cases david with x,
+cases david_h with y,
+cases david_h_h with z,
+use x,
+apply lt_of_lt_of_le,
+-- FIX THIS
+sorry,
+use y,
+sorry,
+use z,
+sorry,
+apply david_h_h_h,
+end
 
 def fin_preim {α : Type} [decidable_eq α] {n : ℕ} (f : fin n → α) (i : α) : finset (fin n) := finset.filter (λ j, f j = i) (fin.fintype n).elems
 
