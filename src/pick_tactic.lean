b7a6d1374ca05912faa3d9efdf83c23274b3e8e8
David E. Narváez <david.narvaez@computer.org>
2023-04-04 16:36:15 -0400

    Adding a pick Tactic
    Initial iteration, the syntax to pick k elements out of a set whose
    cardinality is accordingly bound is as shown in vdw.lean. It will
    define k new bindings, and prove they are pairwise different and
    belong to the set. The status of the tactic is work-in-progress and
    only the assumptions explained above should be trusted to stay in the
    future.

---
+++ src/pick_tactic.lean
@@ -0,0 +1,184 @@
+import data.finset
+
+open interactive (parse)
+open interactive.types (texpr with_ident_list)
+open lean.parser (val ident tk small_nat)
+open tactic.interactive («have»)
+
+lemma pick_one_eq {α : Type} {s : finset α} [decidable_eq α] : 0 < s.card → ∃ (a : α) (t : finset α), (t.card = s.card.pred) ∧ (a ∉ t) ∧ (insert a t = s) := 
+begin
+intro sp,
+have scard: s.card = s.card.pred +1,
+have bb:= nat.eq_zero_or_eq_succ_pred s.card,
+cases bb,
+-- bb in "or"
+rw bb at sp,
+cases sp,
+exact bb,
+rw finset.card_eq_succ at scard,
+rcases scard with ⟨ a,t,x ⟩ ,
+use [a,t],
+--tauto,
+rcases x with ⟨ x1,x2,x3⟩ ,
+-- x in "and"
+-- rcases split more than two assumptions
+split,
+exact x3,
+split,
+exact x1,
+exact x2,
+end
+
+lemma pick_one_lo {α : Type} {s : finset α} [linear_order α] : 0 < s.card → ∃ (a : α) (t : finset α), (t.card = s.card.pred) ∧ (∀ a' ∈ t, a < a') ∧ (insert a t = s) := 
+begin
+
+intro sp,
+rw finset.card_pos at sp,
+let a := s.min' sp, 
+let t := s.erase a,
+use [a,t],
+
+have a_in_s := s.min'_mem sp,
+repeat {split},
+
+{simp [a,t],
+apply finset.card_erase_of_mem,
+apply a_in_s,},
+
+{intro a1,
+intro a1_in_t,
+apply s.min'_lt_of_mem_erase_min' sp a1_in_t,},
+
+apply finset.insert_erase a_in_s,
+
+
+
+
+end
+
+meta def pick_diff (a : expr) (anotint : expr) (info : name × expr) : tactic unit :=
+do {
+  b ← tactic.get_local info.fst,
+  `(_ ∉ %%t) ← tactic.infer_type anotint,
+  neqexpr ← tactic.to_expr ``(%%a ≠ %%b),
+  neqproof ← tactic.to_expr ``(λ x, %%anotint (@eq.subst _ (λ y, y ∈ %%t) _ _ (eq.symm x) %%info.snd)),
+  ineqprooft ← tactic.infer_type neqproof,
+  neqname ← tactic.get_unused_name "neq",
+  tactic.assertv neqname neqexpr neqproof,
+  return()
+}
+
+meta def pick_wrapup (s : expr) (info : name × expr) : tactic unit :=
+do {
+  n ← tactic.get_unused_name "wr",
+  a ← tactic.get_local info.fst,
+  inexpr ← tactic.to_expr ``(%%a ∈ %%s),
+  tactic.assertv n inexpr info.snd,
+  return ()
+}
+
+meta def pick_upgrade (ainst : expr) (info : name × expr) : tactic (name × expr) :=
+do {
+  `(insert %%a %%t = %%s) ← tactic.infer_type ainst,
+  b ← tactic.get_local info.fst,
+  subseqeqexpr ← tactic.to_expr ``(((finset.ext_iff.mp %%ainst) %%b).mp (finset.subset_iff.mp (finset.subset_insert %%a %%t) %%info.snd)),
+  return (info.fst, subseqeqexpr)
+}
+
+-- Invariant: every level returns a list of triples of names where each triple is:
+-- fst: the name of a member obtained in a recursive call
+-- snd: the name of the fact that that member belongs to the rest of the set of this level
+-- It is the responsibility of each level to upgrade the recursive list for the calling level
+meta def pick : ℕ → expr → tactic (list (name × expr))
+| nat.zero bineq := do {
+    tactic.trace "here",
+    tactic.trace bineq,
+    omg ← tactic.infer_type bineq,
+    tactic.trace omg,
+    `(%%b < (finset.card %%s)) ← tactic.infer_type bineq,
+    `(finset %%α) ← tactic.infer_type s,
+    tactic.trace b, tactic.trace s,
+    elemname ← tactic.get_unused_name "a",
+    subsetname ← tactic.get_unused_name "t",
+    atcardname ← tactic.get_unused_name "atcard",
+    anotintname ← tactic.get_unused_name "anotint",
+    ainstname ← tactic.get_unused_name "ainst",
+    match b.to_nat with
+    | some b' := match b' with
+        | nat.zero := do {
+            tactic.rcases none ``(pick_one_eq %%bineq) (tactic.rcases_patt.tuple (list.map tactic.rcases_patt.one [elemname, subsetname, atcardname, anotintname, ainstname]))
+          }
+        | nat.succ b'' := do {
+            tactic.rcases none ``(pick_one_eq (lt_of_le_of_lt (nat.zero_le %%b) %%bineq)) (tactic.rcases_patt.tuple (list.map tactic.rcases_patt.one [elemname, subsetname, atcardname, anotintname, ainstname]))
+          }
+      end
+    | none := tactic.fail "Somehow the bound was not a nat"
+    end,
+    elem ← tactic.get_local elemname,
+    subset ← tactic.get_local subsetname,
+    ainst ← tactic.get_local ainstname,
+    tactic.trace ainst,
+    ainparent ← tactic.to_expr ``(@eq.subst _ (λ x, %%elem ∈ x) _ _ %%ainst (finset.mem_insert_self %%elem %%subset)),
+    return [(elemname, ainparent)]
+  }
+| (nat.succ n) bineq := do {
+    `(%%b < (finset.card %%s)) ← tactic.infer_type bineq,
+    `(finset %%α) ← tactic.infer_type s,
+    tactic.trace b, tactic.trace s,
+    elemname ← tactic.get_unused_name "a",
+    subsetname ← tactic.get_unused_name "t",
+    atcardname ← tactic.get_unused_name "atcard",
+    anotintname ← tactic.get_unused_name "anotint",
+    ainstname ← tactic.get_unused_name "ainst",
+    tactic.rcases none ``(pick_one_eq (lt_of_le_of_lt (nat.zero_le %%b) %%bineq)) (tactic.rcases_patt.tuple (list.map tactic.rcases_patt.one [elemname, subsetname, atcardname, anotintname, ainstname])),
+    subset ← tactic.resolve_name subsetname,
+    atcard ← tactic.resolve_name atcardname,
+    match b.to_nat with
+    | some b' := do {
+        -- We have to trust bpred is b-1 here
+        newineqexpr ← tactic.to_expr ``(%%(reflect b'.pred) < (finset.card %%subset)),
+        newineqproof ← tactic.to_expr ``(@eq.subst nat (λ x, %%(reflect b'.pred) < x) _ _ (eq.symm %%atcard) (nat.pred_lt_pred (nat.succ_ne_zero %%(reflect b'.pred)) %%bineq)),
+        newboundname ← tactic.get_unused_name "newb",
+        tactic.assertv newboundname newineqexpr newineqproof,
+        simpdefault ← simp_lemmas.mk_default,
+        localbound ← tactic.get_local newboundname,
+        tactic.simp_hyp simpdefault [] localbound,
+        localbound ← tactic.get_local newboundname,
+        rec ← pick n localbound,
+        elem ← tactic.get_local elemname,
+        atnotint ← tactic.get_local anotintname,
+        list.mmap' (λ i, pick_diff elem atnotint i) rec,
+        ainst ← tactic.get_local ainstname,
+        rec ← list.mmap (λ i, pick_upgrade ainst i) rec,
+        elem ← tactic.get_local elemname,
+        subset ← tactic.get_local subsetname,
+        ainst ← tactic.get_local ainstname,
+        tactic.trace ainst,
+        ainparent ← tactic.to_expr ``(@eq.subst _ (λ x, %%elem ∈ x) _ _ %%ainst (finset.mem_insert_self %%elem %%subset)),
+        return ((elemname, ainparent) :: rec)
+      }
+    | none := tactic.fail "Somehow the bound was not a nat"
+    end
+  }
+
+meta def tactic.interactive.pick (k : parse small_nat) (stexp : parse (tk "from" *> texpr)) : tactic unit :=
+do
+  ctx ← tactic.local_context, 
+  sexp ← tactic.i_to_expr stexp,
+  ineqexp ← tactic.to_expr ``(_ < (finset.card %%sexp)),
+  exp ← tactic.find_assumption ineqexp,
+  etype ← tactic.infer_type exp,
+  `(%%b < (finset.card %%l)) ← tactic.infer_type exp,
+  match b.to_nat with
+  | some c := if c.succ < k then tactic.fail "Picking too many elements!" else
+              match k with
+              | nat.zero := tactic.fail "Pick at least one element!"
+              | (nat.succ k') := do {
+                  tactic.trace k',
+                  newobjs ← pick k' exp,
+                  list.mmap' tactic.trace newobjs,
+                  list.mmap' (λ i, pick_wrapup l i) newobjs
+                }
+              end
+  | none := tactic.fail ("Assumption " ++ (to_string exp) ++ " is not a good bound.")
+  end
--- src/vdw.lean
+++ src/vdw.lean
@@ -3,19 +3,14 @@ import data.fintype.basic
 import algebra.big_operators.basic
 import data.fin.basic
 import combinatorics.pigeonhole
+
 import tactic
 
+import .pick_tactic
 
 def arithprog(a b c:ℕ):Prop:=∃ k:ℕ, (b=a+k) ∧ (c=b+k)
 
 def vdW_prop (N : ℕ) (r : ℕ) : Prop := ∀ f : ℕ → fin r, ∃ (a b c : fin N), (arithprog a b c) ∧ (f a = f b) ∧ (f b = f c)
-
---def is_lt (n:ℕ )(a : fin n) : (a : ℕ) < n := a.2
-
---def cast_lt (n m:ℕ )(i : fin n) (h : i.1 < m) : fin m := ⟨i.1, h⟩
-
---def cast_le (n m:ℕ )(h : n ≤ m) : fin n ↪o fin m :=
---order_embedding.of_strict_mono (λ a, cast_lt a (lt_of_lt_of_le a.2 h)) $ λ a b h, h
 
 lemma vdW_monotone : ∀ n r, vdW_prop n r → ∀ m, n ≤ m → vdW_prop m r :=
 begin
@@ -30,30 +25,6 @@ use [⟨x, lt_of_lt_of_le x.2 ᾰ_1⟩, ⟨y, lt_of_lt_of_le y.2 ᾰ_1⟩, ⟨z,
 apply xyz_h,
 end
 
-lemma pick_one {α : Type} {s : finset α} [decidable_eq α] : 0 < s.card → ∃ (a : α) (t : finset α), (t.card = s.card.pred) ∧ (a ∉ t) ∧ (insert a t = s) := 
-begin
-intro sp,
-have scard: s.card = s.card.pred +1,
-have bb:= nat.eq_zero_or_eq_succ_pred s.card,
-cases bb,
--- bb in "or"
-rw bb at sp,
-cases sp,
-exact bb,
-rw finset.card_eq_succ at scard,
-rcases scard with ⟨ a,t,x ⟩ ,
-use [a,t],
---tauto,
-rcases x with ⟨ x1,x2,x3⟩ ,
--- x in "and"
--- rcases split more than two assumptions
-split,
-exact x3,
-split,
-exact x1,
-exact x2,
-end
-
 example : ∀ f : fin 5 → fin 2, ∃ a b c, (a ≠ b) ∧ (b ≠ c) ∧ (a ≠ c) ∧ (f a = f b) ∧ (f b = f c) :=
 begin
 intros,
@@ -66,119 +37,11 @@ linarith,},
 --exist y<2 st. the set of x st. f(x)=y have cardinality >2
 have fh' := fintype.exists_lt_card_fiber_of_mul_lt_card f inq,
 cases fh' with y fh'',
-
---so cardinality >0 (so that we can use pick one lemma)
-have fh''_1:0<(finset.filter (λ (x : fin 5), f x = y) finset.univ).card,
-{
-transitivity 2,
-simp,
-assumption,
-},
-
---pick one lemma, want to show {a}+t is the orginal set
-rcases (pick_one fh''_1) with ⟨a,t,tcard,anotint,insert⟩,
-
---try to prove tcard>0 so that we can use pick one lemma again
-have tcard1:1<t.card,
-{rw tcard,
-exact nat.lt_pred_iff.mpr fh'',},
-
-have tcard0:0<t.card,
-{
-transitivity 1,
-simp,
-assumption
-},
-
---try to prove {b}+t2=t
-rcases (pick_one tcard0) with ⟨b,t2,t2card,bnotin2,insert2⟩,
-
-have t2card0:0<t2.card,
-{rw t2card,
-exact nat.lt_pred_iff.mpr tcard1, },
-
---try to prove {c}+t3=t2
-rcases (pick_one t2card0) with ⟨c,t3,t3card,cnotin3,insert3⟩,
-
---we have already pick abc, use abc and try to show they are not equal
-use [a,b,c],
-repeat {split},
-
---a!=b
-{by_contra,
-simp [h, ← insert2] at anotint,
-assumption,},
-
---b!=c
-{by_contra,
-simp [h, ← insert3] at bnotin2,
-assumption,},
-
---a!=c
-{by_contra,
-have fact1:=finset.mem_insert_self c t3,
-rw insert3 at fact1,
-have fact2:=finset.subset_insert b t2,
-rw insert2 at fact2,
-have fact3 := finset.mem_of_subset fact2 fact1,
-rw ← h at fact3,--cases anotint fact3,
-contradiction,},
-
---f(a)=f(b)
-{have amember :=  finset.mem_insert_self a t,
-simp [insert] at amember,
-have b_in_t := finset.mem_insert_self b t2,
-rw insert2 at b_in_t,
-have bmember := finset.mem_of_subset (finset.subset_insert a t)(b_in_t),
-simp [insert] at bmember,
-rw [amember, bmember],},
-
---f(b)=f(c)
-{have b_in_t := finset.mem_insert_self b t2,
-rw insert2 at b_in_t,
-have bmember := finset.mem_of_subset (finset.subset_insert a t)(b_in_t),
-simp [insert] at bmember,
-have c_in_t₂ := finset.mem_insert_self c t3,
-rw insert3 at c_in_t₂,
-have c_in_t := finset.mem_of_subset (finset.subset_insert b t2) c_in_t₂,
-rw insert2 at c_in_t,
-have cmember := finset.mem_of_subset (finset.subset_insert a t)(c_in_t),
-simp [insert] at cmember,
-rw [bmember, cmember],},
-
-
-end
-
-lemma pick_one_lo {α : Type} {s : finset α} [linear_order α] : 0 < s.card → ∃ (a : α) (t : finset α), (t.card = s.card.pred) ∧ (∀ a' ∈ t, a < a') ∧ (insert a t = s) := 
-begin
-
-intro sp,
-rw finset.card_pos at sp,
-let a := s.min' sp, 
-let t := s.erase a,
-use [a,t],
-
-have a_in_s := s.min'_mem sp,
-repeat {split},
-
-{simp [a,t],
-apply finset.card_erase_of_mem,
-apply a_in_s,},
-
-{intro a1,
-intro a1_in_t,
-apply s.min'_lt_of_mem_erase_min' sp a1_in_t,},
-
-apply finset.insert_erase a_in_s,
-
-
-
-
-
+pick 3 from (finset.filter (λ (x : fin 5), f x = y) finset.univ),
+sorry
 end
 
 example : ∀ f : fin 5 → fin 2, ∃ a b c, (a < b) ∧ (b < c) ∧ (f a = f b) ∧ (f b = f c) := 
-
 begin
 intros,
 
@@ -190,102 +53,5 @@ linarith,},
 --exist y<2 st. the set of x st. f(x)=y have cardinality >2
 have fh' := fintype.exists_lt_card_fiber_of_mul_lt_card f inq,
 cases fh' with y fh'',
-
---so cardinality >0 (so that we can use pick one lemma)
-have fh''_1:0<(finset.filter (λ (x : fin 5), f x = y) finset.univ).card,
-{
-transitivity 2,
-simp,
-assumption,
-},
-
---pick one lemma, want to show {a}+t is the orginal set
-rcases (pick_one_lo fh''_1) with ⟨a,t,tcard,anotint,insert1⟩,
-
---try to prove tcard>0 so that we can use pick one lemma again
-have tcard1:1<t.card,
-{rw tcard,
-exact nat.lt_pred_iff.mpr fh'',},
-
-have tcard0:0<t.card,
-{
-transitivity 1,
-simp,
-assumption
-},
-
---try to prove {b}+t2=t
-rcases (pick_one_lo tcard0) with ⟨b,t2,t2card,bnotin2,insert2⟩,
-
-have t2card0:0<t2.card,
-{rw t2card,
-exact nat.lt_pred_iff.mpr tcard1, },
-
---try to prove {c}+t3=t2
-rcases (pick_one_lo t2card0) with ⟨c,t3,t3card,cnotin3,insert3⟩,
-
--- Work around a strange unification problem between different versions of insert
-rw finset.ext_iff at *,
-
---we have already pick abc, use abc and try to show they are not equal
-use [a,b,c],
-repeat {split},
-
---a < b
-{by_contra,
-simp [h, ← insert2] at anotint,
-assumption,},
-
---b < c
-{by_contra,
-simp [h, ← insert3] at bnotin2,
-assumption,},
-
---f(a)=f(b)
-{
--- a ∈ t ∪ {a} => f(a) = y
-rw finset.ext_iff at insert1,
-have amember :=  insert1 a,
-rw finset.mem_insert at amember,
-simp at amember, 
--- b ∈ t ∪ {a} => f(b) = y
-rw finset.ext_iff at insert2,
-have b_in_t := insert2 b,
-rw finset.mem_insert at b_in_t,
-simp at b_in_t,
-have bmember := insert1 b,
-simp [finset.mem_of_subset (finset.subset_insert a t)(b_in_t)] at bmember,
-simp [b_in_t] at bmember,
-rw [amember, bmember],},
-
-{rw finset.ext_iff at insert1,
-rw finset.ext_iff at insert2,
-rw finset.ext_iff at insert3,
-
--- b ∈ t ∪ {a} => f(b) = y
-have b_in_t := insert2 b,
-rw finset.mem_insert at b_in_t,
-simp at b_in_t,
-have bmember := insert1 b,
-simp [finset.mem_of_subset (finset.subset_insert a t)(b_in_t)] at bmember,
-simp [b_in_t] at bmember,
-
--- c ∈ t₂
-have c_in_t₂ := insert3 c, 
-rw finset.mem_insert at c_in_t₂,
-simp at c_in_t₂,
-
--- c ∈ t
-have c_in_t := insert2 c,
-rw finset.mem_insert at c_in_t,
-simp [c_in_t₂]at c_in_t,
-
-
--- c ∈ t ∪ {a} => f(c) = y
-have cmember := insert1 c,
-simp [finset.mem_of_subset (finset.subset_insert a t)(c_in_t)] at cmember,
-simp [c_in_t] at cmember,
-rw [bmember,cmember],},
-
-
+sorry
 end
