03be07b29ce4719e46b489ded70ec11f572b9da7
David E. Narváez <david.narvaez@computer.org>
2023-07-03 12:15:59 -0400

    Adding Comments
    Based on Nikko's comments on an earlier version of the proof.

--- src/vdw.lean
+++ src/vdw.lean
@@ -91,12 +91,17 @@ lemma vdW325 : vdW_prop 325 3 2 :=
 begin
 unfold vdW_prop,
 intros,
+-- f is the color function
+-- Define g: takes any number between 1-33 groups, and return the color combination of that group
 let g : fin 33 → bitvec 5 := λ k, vector.of_fn (λ i, f (5 * k + i) = 0),
+-- There are only 32 types of coloring, thus 32<33
 have fin533 : fintype.card (bitvec 5) • 1 < fintype.card (fin 33),
 simp,
 linarith,
+-- There exists more than 1 blocks that have the same color combination
 have ghyp := fintype.exists_lt_card_fiber_of_mul_lt_card g fin533,
 rcases ghyp with ⟨y₅, y₅hyp⟩,
+-- pick 2 blocks with same color combination
 pick 2 from (finset.filter (λ (x : fin 33), g x = y₅) finset.univ) with block₁ block₂,
 simp at block₁.elem block₂.elem,
 
@@ -135,16 +140,26 @@ fin_cases i; {
   exact notc fbneq0 fb₁b₂,
 },
 
+clear fin533 y₅hyp block₂.elem block₁.elem y₅,
+
+-- let targetfinset be the first three element in block1
 let targetfinset := (insert (5 * block₁.val) (insert (5 * block₁.val + 1) (insert (5 * block₁.val + 2) (∅:(finset ℕ))))),
+-- at least two elememts have the same color
 have fin25 : fintype.card (fin 2) • 1 <  fintype.card ↥targetfinset := by simp,
+-- Define f': takes one of the elemnet in finset ∅, return its color
 let f' : (insert (5 * block₁.val) (insert (5 * block₁.val + 1) (insert (5 * block₁.val + 2) (∅:(finset ℕ))))) → fin 2 := λ k, f k,
+-- There exists more than 1 elements that have the same color
 have fh' := fintype.exists_lt_card_fiber_of_mul_lt_card f' fin25,
 rcases fh' with ⟨c, chyp⟩,
+-- Among the selected three elements, pick two elements that have the same color
 pick 2 from (finset.filter (λ (x : ↥{5 * block₁.val, 5 * block₁.val + 1, 5 * block₁.val + 2}), f' x = c) finset.univ) with a₁ a₂,
 simp at a₁.elem a₂.elem,
+clear fin25 chyp,
 
-have a₁.lt.a₂.cast_bound : ↑a₁ < ↑a₂ := by exact a₁.lt.a₂,
+have a₁.lt.a₂.cast_bound : ↑a₁ < ↑a₂ := by exact a₁.lt.a₂, 
+-- express a2 as 5b2+i and prove
 have out₂ : ∃ i, (↑a₂ = 5 * ↑block₁ + i) ∧ (i < 3),
+-- three cases for a2: i =0,1,2
 rcases a₂.elem.left with rfl | rfl | rfl,
 use 0,
 simp,
@@ -154,7 +169,10 @@ use 2,
 simp,
 rcases out₂ with ⟨i₂, a₂eq, i₂ineq⟩,
 simp [a₂eq] at a₁.lt.a₂.cast_bound,
+
+-- express a1 as 5b1+i and prove
 have out₁ : ∃ i, (↑a₁ = 5 * ↑block₁ + i) ∧ (i < i₂),
+-- three cases for a1: i =0,1,2
 rcases a₁.elem.left with rfl | rfl | rfl,
 use 0,
 simp at a₁.lt.a₂.cast_bound ⊢,
@@ -167,6 +185,8 @@ simp at a₁.lt.a₂.cast_bound ⊢,
 exact a₁.lt.a₂.cast_bound,
 rcases out₁ with ⟨i₁, a₁eq, i₁ineq⟩,
 simp [a₁eq, a₂eq, tsub_add_eq_tsub_tsub],
+clear targetfinset a₁.lt.a₂ a₁.lt.a₂.cast_bound,
+
 let I := i₂ - i₁,
 let B : ℕ := ↑block₂ - ↑block₁,
 have Ibound : i₁ + I < 3,
@@ -174,20 +194,21 @@ change i₁ + (i₂ - i₁) < 3,
 rw ← nat.add_sub_assoc (le_of_lt i₁ineq) i₁,
 simp,
 exact i₂ineq,
+
 have Bbound : ↑block₁ + B < 33,
 change ↑block₁ + (↑block₂ - ↑block₁) < 33,
 have block₁.lt.block₂.cast_bound : ↑block₁ < ↑block₂ := by exact block₁.lt.block₂,
 rw ← nat.add_sub_assoc (le_of_lt block₁.lt.block₂.cast_bound) block₁,
 simp,
-
 have b₂.cast_bound: ↑block₂ < 33 := by exact block₂.property,
 exact b₂.cast_bound,
 
 let a₃ : ℕ := ↑a₁ + (I + I),
-
+-- two cases: same color vs. different color
 cases (fin.decidable_eq 2) (f a₃) (f a₁),
 rotate,
 
+--- Same color case
 -- Case I: 5block₁ + i₁, 5block₁ + i₂, 5block₁ + i₃
 use {start := a₁, diff := I},
 simp,
@@ -204,7 +225,6 @@ fin_cases i; simp [ehyp]; linarith,
 
 -- Prove a₁ a₂ a₃ have same color
 fin_cases i,
---f(a₁) = c
 simp [ehyp], 
 exact a₁.elem.right,
 
@@ -224,9 +244,9 @@ exact a₂.elem.right,
 -- f(a₃) = c
 simp [← a₃, ehyp, h],
 exact a₁.elem.right,
-
 cases (fin.decidable_eq 2) (f (↑a₁ + (I + 5 * B + (I + 5 * B)))) (f a₁),
 rotate,
+
 --Case II: 5block₁ + i₁, 5block₂ + i₂, 5block₃ + i₃
 use {start := a₁, diff := I + 5*B},
 simp,
@@ -241,6 +261,7 @@ cases H with i ehyp,
 split,
 
 have b₁.cast_bound: ↑block₁ < 33 := by exact block₁.property,
+
 --prove <325
 fin_cases i,
 
